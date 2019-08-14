3a0c321e40fa008703a72a8c3c83c482cb9534fb
zm1105 <827556302@qq.com>
2019-08-14 15:38:24 +0800

    常规修改

--- src/main/java/JsonToJava/JsonTest04.java
+++ src/main/java/JsonToJava/JsonTest04.java
@@ -7,12 +7,9 @@ import java.io.IOException;
 
 public class JsonTest04 {
     public static void main(String[] args) throws IOException {
-        String json="{\"name\":\"张三\",\"gender\":\"男\",\"age\":23}";
-        ObjectMapper mapper=new ObjectMapper();
+        String json = "{\"name\":\"张三\",\"gender\":\"男\",\"age\":23}";
+        ObjectMapper mapper = new ObjectMapper();
         Person person = mapper.readValue(json, Person.class);
         System.out.println(person);//Person{name='张三', gender='男', age=23}
-
     }
-
-
 }
